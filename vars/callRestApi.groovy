7da46814431c1e63e1849dec734fb69f80e0cdbc
kum.hoe.lau <kum.hoe.lau@accenture.com>
2021-06-09 17:08:35 +0800

    Add httpClient call

--- vars/callRestApi.groovy
+++ vars/callRestApi.groovy
@@ -1,7 +1,16 @@
+import com.cdancy.jenkins.rest.JenkinsClient
+
 def call(Map config) {
   node() {
     stage('Test') {
       echo 'Hello from callRestApi'
-    }
-  }
+
+      JenkinsClient client = JenkinsClient.builder()
+        .endPoint("http://127.0.0.1:5000/api/service/hello")
+        .build()
+      
+      def systemInfo = client.api().systemApi().systemInfo()
+
+      echo "${systemInfo}"
+     }
 }
