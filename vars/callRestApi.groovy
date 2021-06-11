51f44dec3da3bf493a9d24bb26c8f671a7c47663
kum.hoe.lau <kum.hoe.lau@accenture.com>
2021-06-11 11:08:27 +0800

    Add conditional logic

--- vars/callRestApi.groovy
+++ vars/callRestApi.groovy
@@ -16,6 +16,12 @@ def call(Map config) {
 
       echo "File ${filename}: ${fileExist}"
 
+      if(fileExist) {
+        echo "File exist"
+      } else {
+        echo "File not exist"
+      }
+
       // JenkinsClient client = JenkinsClient.builder()
       //   .endPoint("http://127.0.0.1:5000/api/service/hello")
       //   .build()
