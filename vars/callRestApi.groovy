49424430bda14544e8967b1b1c59318c4f08be00
kum.hoe.lau <kum.hoe.lau@accenture.com>
2021-06-15 11:41:34 +0800

    Updated existsTenant

--- vars/callRestApi.groovy
+++ vars/callRestApi.groovy
@@ -23,7 +23,7 @@ def call(Map config) {
 
         existsTenant = fileExists folder_path
 
-        echo "\tFile ${tenantElement}: ${tenantElement}"
+        echo "\tFile ${tenantElement}: ${existsTenant}"
 
       }
 
