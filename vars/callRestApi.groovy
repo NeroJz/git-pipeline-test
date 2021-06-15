868b2d7f7e55d17df3601f0576aae14ec687084c
kum.hoe.lau <kum.hoe.lau@accenture.com>
2021-06-15 12:05:34 +0800

    Converted to string

--- vars/callRestApi.groovy
+++ vars/callRestApi.groovy
@@ -19,7 +19,7 @@ def call(Map config) {
         def tenantElement = tenant
         echo "Starting Process for Tenant: ${tenantElement}"
 
-        def exists = fileExists "${tenantElement}"
+        def exists = fileExists tenantElement.toString()
 
         echo "\tFile ${tenantElement}: ${exists.toString()}"
 
