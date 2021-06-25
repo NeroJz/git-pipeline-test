a57c23b00bf9437a31ca2a3b99ecaa5a0a1efb63
kum.hoe.lau <kum.hoe.lau@accenture.com>
2021-06-25 12:59:16 +0800

    Check null of tenantname_intg

--- vars/callRestApi.groovy
+++ vars/callRestApi.groovy
@@ -15,7 +15,7 @@ def call(Closure body) {
   if(jobName.endsWith('dev')) {
     tenantName = "${config.tenantname_dev}".trim()
     tenants = tenantName.split(",");
-  } else if(jobName.endsWith('intg') && !config.tenantname_intg?.isEmpty()) {
+  } else if(jobName.endsWith('intg') && config.tenantname_intg != null) {
     echo "\tInside Intg job"
     tenantName = "${config.tenantname_intg}".trim()
     tenants = tenantName.split(",");
