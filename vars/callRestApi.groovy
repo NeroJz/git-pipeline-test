cbc37bbabb9588480343cacec454d1e26097eddc
kum.hoe.lau <kum.hoe.lau@accenture.com>
2021-06-28 16:09:53 +0800

    Check is null

--- vars/callRestApi.groovy
+++ vars/callRestApi.groovy
@@ -29,7 +29,7 @@ def call(Closure body) {
     tenants = tenantName.split(",");
   }
 
-  echo "$tenantName"
+  echo "IS NULL: ${tenantName == null}"
 
   echo "After condition"
   echo "Tenants: ${tenants.toString()}"
