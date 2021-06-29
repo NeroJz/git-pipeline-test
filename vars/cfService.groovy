095d3b911e9f24a267fc9bb527ebc75320a3d02c
kum.hoe.lau <kum.hoe.lau@accenture.com>
2021-06-29 13:58:53 +0800

    Display json file info

--- vars/cfService.groovy
+++ vars/cfService.groovy
@@ -1,3 +1,6 @@
+import jenkins.model.Jenkins
+import groovy.json.*
+
 
 def call(Closure body) {
   def config = [:]
@@ -7,14 +10,49 @@ def call(Closure body) {
 
   pipeline {
     agent any
+    environment {
+      jsonFileName = "${config.jsonFileName}"
+      jsonResetName = 'xsSecurityReset.json'
+    }
     stages {
       stage('Checkout') {
         steps {
           echo "Starting function to clone repository"
 
           checkout scm
+
+          echo "The json filename: ${jsonFileName}"
+
+          echo "Starting function to generate security reset jason"
+          createResetJson("${jsonFileName}") //function call for creating reset json
+          def jsonContents = readJSON file: jsonFileName
+          def jsonReset = readJSON file: jsonResetName
+
+          def nonEnvAwareValue = jsonContents.xsappname
+          def nonEnvResetValue = jsonReset.xsappname
+
+          echo "${nonEnvAwareValue}"
+          echo "\n"
+          echo "${nonEnvResetValue}"
         }
       }
     }
+  }
+}
+
+def createResetJson (jsonFileName){
+
+  script{
+      def jsonContentsReset = readJSON file: jsonFileName
+      if (jsonContentsReset.has('role-collections')){
+          jsonContentsReset.remove('role-collections')
+          def jsonStr = JsonOutput.prettyPrint(JsonOutput.toJson(jsonContentsReset))
+          writeFile(file: 'xsSecurityReset.json', text: jsonStr)
+
+          sh 'cat xsSecurityReset.json'
+      }
+      else {
+          throw new Exception("Build failed as role-collections not found under ${jsonFileName}")
+      }
   }
 }
