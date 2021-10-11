2cb6a90e1adfdc300bf1fd892b3e2e2ae1ec780b
kum.hoe.lau <kum.hoe.lau@accenture.com>
2021-10-11 22:17:01 +0800

    Added deployTenantDB.groovy

---
+++ deployTenantDB.groovy
@@ -0,0 +1,121 @@
+import groovy.json.JsonSlurperClassic
+
+def call(Closure body) {
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  def isSkipped = false
+
+  // if(config.tenantname_dev == null) {
+  //   error 'Error: tenantname_dev must be provided from Jenkins!'
+  // }
+  
+  def tenantName = "${config.tenantname_dev}".trim()
+  String[] tenants = tenantName.split(",");
+
+  def jobName = "${env.JOB_NAME}".replace('%2F', '_')
+
+  echo "Config: ${config.tenantname_intg?.trim()}"
+  echo "Before condition"
+  echo "Tenants: ${tenants.toString()}"
+
+  if(jobName.endsWith('dev')) {
+    echo "\tInside Dev job"
+    tenantName = "${config.tenantname_dev}".trim()
+    tenants = tenantName.split(",");
+  } else if(jobName.endsWith('intg') && config.tenantname_intg != null) {
+    echo "\tInside Intg job"
+    tenantName = "${config.tenantname_intg}".trim()
+    tenants = tenantName.split(",");
+  }
+
+  echo "IS NULL: ${tenantName == 'null'}"
+
+  echo "After condition"
+  echo "Tenants: ${tenants.toString()}"
+
+  node() {
+    stage('Checkout') {
+      checkout scm
+
+      if(tenantName == 'null') {
+        isSkipped = false
+        echo 'Tenant name is empty. Skip the Checkout Stage.'
+        return
+      }
+
+      echo "Processing Checkout"
+    }
+    stage('Step 2') {
+      if(tenantName == 'null') {
+        echo 'Step 2 Tenant name is empty. Skip the rest of the pipeline.'
+        currentBuild.getRawBuild().getExecutor().interrupt(Result.SUCCESS)
+        return
+      }
+
+      echo "Processing Step 2"
+    }
+    stage('Step 3') {
+      if(tenantName == 'null') {
+        echo 'Step 3 Tenant name is empty. Skip the rest of the pipeline.'
+        currentBuild.getRawBuild().getExecutor().interrupt(Result.SUCCESS)
+        return
+      }
+
+      echo "Processing Step 3"
+    }
+    stage('Test Tenantname Based on Job Name') {
+      echo "$env.JOB_NAME"
+    }
+    // stage('Test Directory') {
+    //   stdout = sh(returnStdout: true, script: "ls -l")
+    //   echo "${stdout.trim()}"
+
+    //   for(String tenant : tenants) {
+    //     String tenantElement = "${tenant}"
+    //     echo "Starting Process for Tenant: ${tenantElement}"
+
+    //     def replacedTenant = getReplacedTenantName(tenantElement)
+
+    //     echo "\tgetReplacedTenantName ---> ${replacedTenant}"
+
+    //     def exists = fileExists replacedTenant
+
+    //     echo "\tFile ${tenantElement}: ${exists.toString()}"
+
+    //   }
+    // }
+    // stage('Patch') {
+    //   withCredentials([usernamePassword(credentialsId: 'PRTG_CREDENTIAL', 
+    //     usernameVariable: 'PRTG_USR', 
+    //     passwordVariable: 'PRTG_PWD')]) {
+    //     // echo PRTG_USR
+    //     // echo PRTG_PWD
+
+    //     for(String tenant : tenants) {
+    //       def data = '{"prtg.user":"' + PRTG_USR + '","prtg.password":"' + PRTG_PWD +'", "tenant":"'+ tenant.trim() +'"}'
+    //       def command = 'curl -d \''+ data + '\' -H "Content-Type: application/json" -X POST http://host.docker.internal:5000/api/service/hello'
+
+    //       echo command
+          
+    //       def stdout = sh(returnStdout: true, script: command)
+
+    //       def jsonObj = new JsonSlurperClassic().parseText(stdout.trim())
+
+    //       echo "${jsonObj.status}"
+
+    //       echo "${stdout.trim()}"
+    //     }
+    //   }
+    // }
+  }
+}
+
+
+def getReplacedTenantName(String tenant) {
+  def replacedTenant = tenant.trim().replaceAll(/(-dev|-intg)/, "")
+
+  return replacedTenant
+}
