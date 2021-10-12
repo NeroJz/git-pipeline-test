6c82e42e65aa400ebce3824dbfa5f08cefb9fd43
kum.hoe.lau <kum.hoe.lau@accenture.com>
2021-10-12 23:02:13 +0800

    List the file

--- src/com/accenture/newspage/MultiTenant.groovy
+++ src/com/accenture/newspage/MultiTenant.groovy
@@ -8,32 +8,6 @@ def processTenantFromFolder(String appName, String replacedTenantFolderName, Str
   //sh "cp -Rv ${tenantName}/* ${appName}"
   pwd = sh(returnStdout:true,script : 'pwd').replaceAll("[\\n ]", "")
   dir (replacedTenantFolderName) {
-    echo "\t${tenantNameElement} found. Running within ${pwd}"
-
-    /* Hide for temporary */
-    command =  $/ find ./tenant/ -maxdepth 5 -type f -not -path '*/.*' | sort /$ 
-    
-    listFiles = sh(returnStdout:true, script: command)
-    echo listFiles
-    listArray = listFiles.split('\n')
-    size = listArray.size()
-    if ({listArray.length >= 1}){
-      for (int i = 0; i<size; i++){
-        //For .csv files 
-        def path = (listArray[i] - (listArray[i].substring(listArray[i].lastIndexOf('/') + 1).replaceAll("[\\n ]", ""))).substring(1)
-        
-        // Original logic move to copy_and_merge function
-
-        // Test Copy
-        fileName = listArray[i].substring(listArray[i].lastIndexOf('/') + 1).replaceAll("[\\n ]", "")
-        path = "../../${appName}/src${path}"
-        exists = sh (returnStatus:true, script: "find ${path} -name $fileName | grep .")
-        if ( exists != 0 ){
-          sh (returnStdout:true, script: "cp ${listArray[i]} ${path}")
-          sh "ls -l $path"
-        }
-
-      }
-    }
+    sh 'ls l'
   }
 }
